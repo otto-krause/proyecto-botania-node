c30b39a0297305510f92ebea32cee138ff9bbead
Maikhol <maikholsozaki@gmail.com>
2019-08-23 10:13:21 -0300

    Version Beta 1.0.0

--- src/router/buscador.js
+++ src/router/buscador.js
@@ -3,7 +3,7 @@ const pool = require('../database');
 
 router.get('/',async(req,res,next)=>{
     //const {idPropagacion} = req.params;
-    const propagaciones = await pool.query('SELECT CodPPRenglon, Fecha, Nombre, CodPPRenglon.Des FROM (CodPPRenglon JOIN CodItemPP ON (CodPPRenglon.CodItemPP = CodItemPP.IdItemPP))')
+    const propagaciones = await pool.query('SELECT CodPPRenglon, Fecha, Nombre, CodPPRenglon.Des FROM (CodPPRenglon JOIN CodItemPP ON (CodPPRenglon.CodItemPP = CodItemPP.IdItemPP)) LIMIT 10')
     //const propagaciones = await pool.query('SELECT CodPPRenglon, Fecha, Nombre, CodPPRenglon.Des FROM (CodPPRenglon JOIN CodItemPP ON (CodPPRenglon.CodItemPP = CodItemPP.IdItemPP)) WHERE (CodPP IN (SELECT IdPPropagacion FROM CodPPropagacion) AND CodPP = ?)')
     .catch(err=> next(err));
     console.log(propagaciones);
--- src/router/buscadorAvanzado.js
+++ src/router/buscadorAvanzado.js
@@ -1,9 +1,60 @@
 const router = require('express').Router();
 const pool = require('../database');
 
-router.get('/BuscadorAvanzado',async(req,res,next)=>{
-    const propagaciones = await pool.query('SELECT * FROM Propagacion WHERE idPropagacion = ?',4) //Acá va la consulta que va a hacer Guille
+router.get('/',async(req,res,next)=>{
+    const propagaciones = await pool.query(`SELECT propRengItemTec.idPPropagacion, ingAlta.Nombre, propRengItemTec.ApellidoTec, propRengItemTec.NombreTec, propRengItemTec.ApellidoAsist, propRengItemTec.NombreAsist, propRengItemTec.Objetivo, ingAlta.Metodo, ingAlta.idIngreso, ingAlta.Legado, ingAlta.FRecoleccion, propRengItemTec.Fecha, propRengItemTec.ItemCierre
+    FROM (
+            SELECT propRengItemAsist.idPPropagacion, propRengItemAsist.Objetivo, propRengItemAsist.CodIngreso0, tecnico.Apellido AS ApellidoTec, tecnico.Nombre AS NombreTec, propRengItemAsist.ApellidoAsist, propRengItemAsist.NombreAsist, propRengItemAsist.CodPP, propRengItemAsist.Fecha, propRengItemAsist.ItemCierre
+            FROM (
+                    SELECT propRengItem.IdPPropagacion, propRengItem.Objetivo, propRengItem.CodIngreso0, propRengItem.CodTecnico, asistente.Nombre AS NombreAsist, asistente.Apellido AS ApellidoAsist, propRengItem.ItemCierre, propRengItem.Fecha, propRengItem.CodPP
+                    FROM (
+                            SELECT propagacion.IdPPropagacion, propagacion.Objetivo, propagacion.CodIngreso0, propagacion.CodTecnico, propagacion.CodAsistente, renglonItem.CodPP, renglonItem.Fecha, renglonItem.ItemCierre
+                            FROM (
+                                    SELECT IdPPropagacion, Objetivo, CodIngreso0, CodTecnico, CodAsistente
+                                    FROM codppropagacion
+                                    /*WHERE FAltaTP BETWEEN aca salame and */
+                                 ) AS propagacion
+                            INNER JOIN (
+                                          SELECT renglon.CodPP, (CAST(renglon.Fecha AS DATE)) AS Fecha, item.ItemCierre
+                                          FROM (
+                                                  SELECT CodPP, CodItemPP, Fecha
+                                                  FROM CodPPRenglon
+                                               ) AS renglon
+                                          INNER JOIN (
+                                                        SELECT idItemPP, ItemCierre
+                                                        FROM CodItemPP
+                                                          WHERE ItemCierre = true /*Esto se codea si el tipo quiere ver todos o los en curso*/
+                                                     ) AS item
+                                          ON renglon.CodItemPP = item.idItemPP
+                                       ) AS renglonItem
+                            ON propagacion.idPPropagacion = renglonItem.codPP
+                         ) AS propRengItem
+                    INNER JOIN (
+                                  SELECT idTecnico, Apellido, Nombre FROM CodTecnico
+                               ) AS asistente
+                    ON propRengItem.CodAsistente = asistente.idTecnico
+                 ) AS propRengItemAsist
+            INNER JOIN (
+                          SELECT idTecnico, Apellido, Nombre FROM CodTecnico
+                       ) AS tecnico
+            ON propRengItemAsist.CodTecnico = tecnico.idTecnico
+         ) AS propRengItemTec
+    INNER JOIN (
+                  SELECT ingreso.idIngreso, ingreso.Nombre, alta.Nombre AS Metodo, ingreso.Legado, ingreso.FRecoleccion
+                  FROM (
+                            SELECT idIngreso, Nombre, CodTipoAlta, Legado, (CAST(FRecoleccion AS DATE)) AS FRecoleccion
+                            FROM codingreso
+                       ) AS ingreso
+                  INNER JOIN (
+                                  SELECT idAlta, Nombre
+                                  FROM codAlta
+                               ) AS alta
+                  ON ingreso.CodTipoAlta = alta.idAlta
+               ) AS ingAlta
+    ON propRengItemTec.CodIngreso0 = ingAlta.idIngreso
+    GROUP BY 1,2,3,4,5,6,7,8,9,10,11,12,13 LIMIT 10`)
     .catch(err=> next(err));
+    console.log(propagaciones);
     res.send(propagaciones);
 });
 
